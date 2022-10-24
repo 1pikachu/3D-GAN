0f74a2e52ec51f73ea88605a5b52b8fc7783330d
Du, Jun <jun.du@intel.com>
2022-10-24 05:38:51 +0000

    fix model

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ pip install scikit-image tensorboardX visdom==0.2.1
 mkdir -p outputs/dcgan
 cp -r /home2/pytorch-broad-models/3D-GAN/dcgan_pretrained/first_test outputs/dcgan/.
 
+cd src
 python main.py --test=True --num_iter 200 --num_warmup 20 --jit --channels_last 1 --device cuda --precision float16 --profile
